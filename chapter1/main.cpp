b112673584c7804522932e3f27d4d14d73239602
Sangram Barge <sangrambarge098@gmail.com>
2023-10-04 19:59:44 +0530

    hello world

---
+++ chapter1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void line(), greet();
+
+int main() {
+  line();
+  greet();
+  line();
+  return 0;
+}
+
+void line() {
+  cout << "_______________________________________________" << endl;
+}
+
+void greet() { cout << "Hello everynyan, How aa u, fine sthank you" << endl; }
